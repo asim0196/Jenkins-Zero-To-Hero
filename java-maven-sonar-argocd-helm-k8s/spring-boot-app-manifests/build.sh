6c8ff35b2f3ab2359840f7a38f7e29b3fb39339a
asim0196 <112340466+asim0196@users.noreply.github.com>
2023-10-30 19:24:27 +0530

    Update build.sh

--- java-maven-sonar-argocd-helm-k8s/spring-boot-app-manifests/build.sh
+++ java-maven-sonar-argocd-helm-k8s/spring-boot-app-manifests/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 BUILD_NUMBER=${BUILD_NUMBER}
 let PRE_BUILD=$BUILD_NUMBER-1
-sed -i "s/cicd:$PRE_BUILD/cicd:$BUILD_NUMBER/g" deployment.yml
+sed -i "s/cicd:$PRE_BUILD/cicd:$BUILD_NUMBER/g" java-maven-sonar-argocd-helm-k8s/spring-boot-app-manifests/deployment.yml
